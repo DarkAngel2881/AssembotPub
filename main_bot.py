1a5f99a3257282190286afe59eb696a7f3387fba
DarkAngel2881 <100215802+DarkAngel2881@users.noreply.github.com>
2022-11-28 21:28:22 +0100

    Update main_bot.py

--- main_bot.py
+++ main_bot.py
@@ -1,79 +1,40 @@
-import Costants as keys 
+import Costants as keys
+from telegram.ext import *
+import Responses as R
 
- from telegram.ext import * 
+print("Bot initialized...")
 
- import Responses as R 
+def start_command(update, context):
+    update.message.reply_text('Porco il mago del codice buongiorno!')
 
-  
 
- print("Bot initialized...") 
+def help_command(update, context):
+    update.message.reply_text('This is what you need to know: ')
 
-  
 
- def start_command(update): 
+def handle_message(update, context):
+    text = str(update.message.text).lower()
+    response = R.sample_responses(text)
 
-     update.message.reply_text('Ask something!') 
+    update.message.reply_text(response)
 
-  
 
-  
+def error(update, context):
+    print(f"Update {update} caused error {context.error}")
 
- def help_command(update): 
 
-     update.message.reply_text('This is what you need to know: ') 
+def main():
+    updater = Updater(keys.API_KEY, use_context=True)
+    dp = updater.dispatcher
 
-  
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("help", help_command))
 
-  
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
 
- def handle_message(update): 
+    dp.add_error_handler(error)
 
-     text = str(update.message.text).lower() 
+    updater.start_polling()
+    updater.idle()
 
-     response = R.sample_responses(text) 
-
-  
-
-     update.message.reply_text(response) 
-
-  
-
-  
-
- def error(update, context): 
-
-     print(f"Update {update} caused error {context.error}") 
-
-  
-
-  
-
- def main(): 
-
-     updater = Updater(keys.API_KEY, use_context=True) 
-
-     dp = updater.dispatcher 
-
-  
-
-     dp.add_handler(CommandHandler("start", start_command)) 
-
-     dp.add_handler(CommandHandler("help", start_command)) 
-
-  
-
-     dp.add_handler(MessageHandler(Filters.text, handle_message)) 
-
-  
-
-     dp.add_error_handler(error) 
-
-  
-
-     updater.start_polling() 
-
-     updater.idle() 
-
-  
-
- main()
+main()
